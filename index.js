131bc4b8271308102fa07368016aa9b770fa2b99
Cihad Turhan <cihadturhan@users.noreply.github.com>
2017-04-07 12:41:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ module.exports = {
   MESSAGE:      'MESSAGE',
   DEVICE_LIST:  'DEVICE_LIST',  
   MESSAGE_LIST: 'MESSAGE_LIST',
-  ERROR: 'ERROR'
+  ERROR: 'ERROR',
+  DB: {
+    ValidationException: 'Invalid Query',
+    InvalidArnException: 'Unknown Error',
+    ResourceNotFoundException: 'Request Resource Not Found',
+    InvalidNextTokenException: 'Invalid Next Token',
+    AccessDeniedException: 'Access Denied',
+    NotNodeException: 'Not Node'
+  }
 }
